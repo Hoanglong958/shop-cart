c7be1075f4da5d3604eeb4261f556e1c8263b6f8
Hoanglong958 <hoanglongh911@gmail.com>
2025-09-23 22:15:17 +0700

    update code

---
+++ src/services/api.js
@@ -0,0 +1,11 @@
+// src/services/api.js
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "http://localhost:5000/api", // đổi thành URL backend của bạn
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+export default api;
